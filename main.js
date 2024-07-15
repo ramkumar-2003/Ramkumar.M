d062eff7adda7dff97f0a9f098ba64805d1bba42
Ramkumar M <128789857+ramkumar-2003@users.noreply.github.com>
2024-07-15 12:32:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var Typed= new Typed(".text", {
+    strings:["Frontend Developer" , "Backend Developer" , "Web Developer"],
+    typespeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+})
